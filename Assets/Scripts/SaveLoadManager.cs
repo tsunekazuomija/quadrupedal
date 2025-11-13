fd10489f832e90e930565159c2355a3587b099dd
tsuenakzuomija <tsunekazuomija@gmail.com>
2025-11-13 16:02:28 +0900

    updated directory to save data from persistentDataPath to Application.dataPath + "RobotData" directory

--- Assets/Scripts/SaveLoadManager.cs
+++ Assets/Scripts/SaveLoadManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
 public class SaveLoadManager : MonoBehaviour {
 
@@ -19,6 +20,8 @@ public class SaveLoadManager : MonoBehaviour {
         }
     }
 
+    private readonly string _saveDirectory = System.IO.Path.Combine(Application.dataPath, "RobotData");
+
     void Awake() {
         if (instance == null) {
             instance = this;
@@ -30,17 +33,31 @@ public class SaveLoadManager : MonoBehaviour {
 
     public void SaveRobotData(List<GeneData> geneDataList) {
         string jsonData = JsonUtility.ToJson(new GeneDataList(geneDataList));
-        string filePath = System.IO.Path.Combine(Application.persistentDataPath, "robots_save_data.json");
-        System.IO.File.WriteAllText(filePath, jsonData);
-        // Debug.Log("Persistent Data Path: " + Application.persistentDataPath);
+        string filePath = System.IO.Path.Combine(_saveDirectory, "robots_save_data.json");
+#if UNITY_EDITOR_WIN
+        filePath = filePath.Replace("\\", "/");
+#endif
+        try
+        {
+            System.IO.File.WriteAllText(filePath, jsonData);
+            Debug.Log($"Saved {geneDataList.Count} robots to {filePath}");
+        }
+        catch (Exception e)
+        {
+            Debug.Log($"Failed to save data to {filePath}: {e.Message}");
+            return;
+        }
     }
 
     public GeneDataList LoadRobotData() {
-        string filePath = System.IO.Path.Combine(Application.persistentDataPath, "robots_save_data.json");
+        string filePath = System.IO.Path.Combine(_saveDirectory, "robots_save_data.json");
+#if UNITY_EDITOR_WIN
+        filePath = filePath.Replace("\\", "/");
+#endif
         if (System.IO.File.Exists(filePath)) {
             string jsonData = System.IO.File.ReadAllText(filePath);
             GeneDataList geneDataList = JsonUtility.FromJson<GeneDataList>(jsonData);
-            // Debug.Log("Loaded " + geneDataList.geneDatas.Count + " robots from " + filePath);
+            Debug.Log("Loaded " + geneDataList.geneDatas.Count + " robots from " + filePath);
             return geneDataList;
         } else {
             Debug.Log("Save data file not found in " + filePath);
